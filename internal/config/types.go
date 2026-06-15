b34217601e2d28fb0bc68be6824e590b20656602
brsyuksel <brsyuksel@gmail.com>
2026-06-15 22:47:08 +0000

    fix: enable all job schedule options by default

--- internal/config/types.go
+++ internal/config/types.go
@@ -16,9 +16,9 @@ type JobSchedule struct {
 func DefaultConfig() GlobalConfig {
 	return GlobalConfig{
 		Backup:   JobSchedule{Enabled: true, Time: "04:00"},
-		Snapshot: JobSchedule{Enabled: false, Time: "03:00"},
-		Defrag:   JobSchedule{Enabled: false, Time: "04:00"},
-		Scrub:    JobSchedule{Enabled: false, Time: "05:00"},
-		Balance:  JobSchedule{Enabled: false, Time: "01:00"},
+		Snapshot: JobSchedule{Enabled: true, Time: "03:00"},
+		Defrag:   JobSchedule{Enabled: true, Time: "04:00"},
+		Scrub:    JobSchedule{Enabled: true, Time: "05:00"},
+		Balance:  JobSchedule{Enabled: true, Time: "01:00"},
 	}
 }
